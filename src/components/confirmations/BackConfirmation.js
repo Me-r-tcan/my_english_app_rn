8060d15f0f9b543a61a3863b9a03b69f83db0e5d
Me-r-tcan <mertcanbas35@gmail.com>
2021-08-14 00:49:17 +0300

    Make BackConfirmation from AppAlert component

---
+++ src/components/confirmations/BackConfirmation.js
@@ -0,0 +1,44 @@
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigation } from "@react-navigation/native";
+
+import AppAlert from "../AppAlert";
+import { closeBackConfirmation } from "../../store/confirmations";
+
+const BackConfirmation = () => {
+  const dispatch = useDispatch();
+  const navigation = useNavigation();
+
+  const { BackConfirmationOpen } = useSelector(
+    (state) => state.entities.confirmations
+  );
+
+  const onPressPositiveButton = () => {
+    dispatch(closeBackConfirmation());
+    navigation.goBack();
+  };
+
+  const onPressNegativeButton = () => {
+    dispatch(closeBackConfirmation());
+  };
+
+  return (
+    <AppAlert
+      displayAlert={BackConfirmationOpen}
+      alertTitleText='UYARI'
+      alertMessageText='Geri dönmek istediğinize emin misiniz?'
+      positiveButtonText='ONAYLA'
+      negativeButtonText='İPTAL'
+      onPressPositiveButton={onPressPositiveButton}
+      onPressNegativeButton={onPressNegativeButton}
+      positiveButtonColor='danger'
+      negativeButtonColor='silver'
+      textColors='black'
+      backgroundColor='gray'
+      outerBackgroundColor='transparentGray'
+      height='22%'
+    />
+  );
+};
+
+export default BackConfirmation;
--- src/components/headers/HeaderBackButton.js
+++ src/components/headers/HeaderBackButton.js
@@ -1,20 +1,27 @@
 import React from "react";
 import { Entypo } from "@expo/vector-icons";
-import { useNavigation } from "@react-navigation/native";
+import { useDispatch } from "react-redux";
 
+import BackConfirmation from "../confirmations/BackConfirmation";
 import colors from "../../config/colors";
 
+import { openBackConfirmation } from "../../store/confirmations";
+
 const HeaderBackButton = () => {
-  const navigation = useNavigation();
+  const dispatch = useDispatch();
 
   return (
-    <Entypo
-      name='cross'
-      size={30}
-      color={colors.danger}
-      onPress={() => navigation.goBack()}
-      style={{ paddingLeft: 10 }}
-    />
+    <>
+      <Entypo
+        name='cross'
+        size={30}
+        color={colors.danger}
+        onPress={() => dispatch(openBackConfirmation())}
+        style={{ paddingLeft: 10 }}
+      />
+
+      <BackConfirmation />
+    </>
   );
 };
 
