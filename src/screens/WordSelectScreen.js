23d5c55fd3b340936c1fe52d29e12678cf09d9bd
Me-r-tcan <mertcanbas35@gmail.com>
2021-09-01 04:50:07 +0300

    Add WordSelectScreen

--- src/screens/WordSelectScreen.js
+++ src/screens/WordSelectScreen.js
@@ -1,14 +1,50 @@
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import React, { useEffect } from "react";
+import { StyleSheet, View } from "react-native";
+import { useDispatch, useSelector } from "react-redux";
 
-const WordSelectScreen = () => {
+import AppActivityIndicator from "../components/AppActivityIndicator";
+import BackgroundImage from "../components/BackgroundImage";
+import EducationCarousel from "../components/carousel/EducationCarousel";
+import SelectingArea from "../components/carousel/SelectingArea";
+import CarouselWordSelectItem from "../components/carousel/CarouselWordSelectItem";
+
+import { loadSelectableWords } from "../store/words";
+
+const WordSelectScreen = ({ route }) => {
+  const dispatch = useDispatch();
+
+  const { subjectId, subjectOptionId, progress } = route.params;
+
+  useEffect(() => {
+    dispatch(loadSelectableWords(subjectId, progress));
+  }, []);
+
+  // Burda words listesi kullanılacak - seçenekleri belirlemek için selectableWords listesi kullanılacak
+  const { words, loading } = useSelector((state) => state.entities.words);
+
   return (
-    <View>
-      <Text>WordSelectScreen</Text>
-    </View>
+    <>
+      <BackgroundImage source={require("../assets/images/blue.png")} />
+
+      <AppActivityIndicator visible={loading} />
+
+      <View style={styles.container}>
+        <EducationCarousel
+          slides={words}
+          subjectOptionId={subjectOptionId}
+          scrollEnabled={false}
+          AreaComponent={SelectingArea}
+          CarouselItemComponent={CarouselWordSelectItem}
+        />
+      </View>
+    </>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
 
 export default WordSelectScreen;
